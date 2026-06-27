16bab396e823371d65d1f6d3ac22bdef7e43db51
Israel Saari <isaar@users.noreply.github.com>
2026-06-27 00:02:39 -0400

    Publish telemetry roll animation

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vaportrail-shell-20260626235043';
+const CACHE_NAME = 'vaportrail-shell-20260627000113';
 const APP_SHELL = ['/vaportrail/', '/vaportrail/manifest.webmanifest', '/vaportrail/icon.png', '/vaportrail/favicon.png'];
 
 self.addEventListener('install', (event) => {
