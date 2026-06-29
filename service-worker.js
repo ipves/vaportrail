74e46d2437b30981eda437398e01233f27adbc74
Israel Saari <isaar@users.noreply.github.com>
2026-06-29 10:00:36 -0400

    Publish VaporTrail PWA

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vaportrail-shell-20260629095809';
+const CACHE_NAME = 'vaportrail-shell-20260629100035';
 const APP_SHELL = ['/vaportrail/', '/vaportrail/manifest.webmanifest', '/vaportrail/icon.png', '/vaportrail/favicon.png'];
 
 self.addEventListener('install', (event) => {
